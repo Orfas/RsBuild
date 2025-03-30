bed9ad22f389495dea6222374083bf5e160ae48b
Orfas <47422008+Orfas@users.noreply.github.com>
2025-03-30 13:34:26 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import { Component } from '@components/index';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
